493b05f56e59597a272888b4011433935ade8ef1
looserakki <70113298+looserakki@users.noreply.github.com>
2021-07-17 08:18:37 +0530

    Update sample_config.py

--- lycia/sample_config.py
+++ lycia/sample_config.py
@@ -1,4 +1,4 @@
 class Config(object):
-    API_ID = int("api_id")
-    API_HASH = "api_hash"
-    TOKEN = "token"
+    API_ID = int("api_id", "")
+    API_HASH = "api_hash", ""
+    TOKEN = "token", ""
